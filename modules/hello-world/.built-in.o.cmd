a28b0f78ed74c5481c448ca3ed13b3b4d8938829
MadCoderDev <civokxf250@gmail.com>
2016-03-07 20:17:24 +0100

    removed useless files

--- modules/hello-world/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/madcoder/Programmazione/C/kernel-modules/modules/hello-world/built-in.o :=  rm -f /home/madcoder/Programmazione/C/kernel-modules/modules/hello-world/built-in.o; ar rcsD /home/madcoder/Programmazione/C/kernel-modules/modules/hello-world/built-in.o
--- modules/hello-world/.main.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/madcoder/Programmazione/C/kernel-modules/modules/hello-world/main.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/madcoder/Programmazione/C/kernel-modules/modules/hello-world/main.ko /home/madcoder/Programmazione/C/kernel-modules/modules/hello-world/main.o /home/madcoder/Programmazione/C/kernel-modules/modules/hello-world/main.mod.o
